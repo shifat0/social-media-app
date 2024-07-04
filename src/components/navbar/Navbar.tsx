1f60b16e99985fcfdc075feceba10fba175d1973
shifat0 <sifuisu3@gmail.com>
2024-07-05 00:52:26 +0600

    icon component created and navbar modified

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,42 @@
+import Icon from "@/lib/icon";
 import React from "react";
 
 type Props = {};
 
+const navData = [
+  {
+    name: "Home",
+    iconName: <Icon name="house" />,
+    link: "/",
+  },
+  {
+    name: "Home",
+    iconName: <Icon name="house" />,
+    link: "/",
+  },
+  {
+    name: "Home",
+    iconName: <Icon name="house" />,
+    link: "/",
+  },
+  {
+    name: "Home",
+    iconName: <Icon name="house" />,
+    link: "/",
+  },
+];
+
 export default function Navbar({}: Props) {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="flex items-center justify-between">
+        {navData?.map((navItem, index) => (
+          <>
+            <span>{navItem?.name}</span>
+            <span>{navItem?.iconName}</span>
+          </>
+        ))}
+      </div>
+    </nav>
+  );
 }
