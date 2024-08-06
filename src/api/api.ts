3a318560cdde95c53fbe1f4fcdf149311f6dcc53
shifat0 <sifuisu3@gmail.com>
2024-08-06 20:11:49 +0600

    auth page is created

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,22 +1,26 @@
 import { api } from "@/lib/apiConfig";
-import { AxiosResponse } from "axios";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export const getData = async <T>(
   endpoint: string,
   params: Record<string, any> = {}
 ): Promise<T> => {
   const response: AxiosResponse<T> = await api.get(endpoint, { params });
+
   return response.data;
 };
 
 export const postData = async <T>(
   endpoint: string,
   payload: any,
-  params?: Record<string, any>
+  params?: Record<string, any>,
+  requestOptions?: AxiosRequestConfig
 ): Promise<T> => {
   const response: AxiosResponse<T> = await api.post(endpoint, payload, {
     params,
+    ...requestOptions,
   });
+
   return response.data;
 };
 
@@ -28,6 +32,7 @@ export const updateData = async <T>(
   const response: AxiosResponse<T> = await api.put(endpoint, payload, {
     params,
   });
+
   return response.data;
 };
 
@@ -36,5 +41,6 @@ export const deleteData = async <T>(
   params: Record<string, any> = {}
 ): Promise<T> => {
   const response: AxiosResponse<T> = await api.delete(endpoint, { params });
+
   return response.data;
 };
