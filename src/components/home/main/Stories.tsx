da096dba9538f8485b4568a24d7d49c780bd32ab
shifat0 <sifuisu3@gmail.com>
2024-09-01 21:47:36 +0600

    modified stories

--- src/components/home/main/Stories.tsx
+++ src/components/home/main/Stories.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export default function Stories({}: Props) {
   return (
-    <div className="bg-primary-foreground p-2">
+    <div className="bg-primary-foreground p-2 rounded-lg">
       <div className="w-[150px] h-[200px] rounded-lg bg-secondary">
         <div className="relative w-full h-2/3">
           <Image
@@ -16,11 +16,12 @@ export default function Stories({}: Props) {
             className="object-cover rounded-t-lg"
           />
         </div>
+
         <div className="h-1/3 relative text-center flex items-center justify-center">
-          <Icon
-            name="circle-plus"
-            className="absolute -top-3 left-1/2 transform -trnaslate-x-1/2 border border-secondary"
-          />
+          <div className="absolute -top-3 left-1/2 transform -translate-x-1/2 bg-secondary size-8 rounded-full p-1 flex items-center justify-center">
+            <Icon name="circle-plus" />
+          </div>
+
           <span>Create Your Story</span>
         </div>
       </div>
